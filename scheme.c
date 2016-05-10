f3f1c10068911ff6f1b19656e409d1de79fb7c39
wanghao1891 <wanghao1891@126.com>
2016-05-10 15:27:47 +0800

    can process nested node.

--- scheme.c
+++ scheme.c
@@ -8,6 +8,7 @@
 //#define START 0
 #define IN 1 // inside a word
 #define OUT 0 // outside a word
+#define END 2 // end for a node
 
 typedef struct ELEMENT {
   int type; //1, number; 2, node
@@ -30,13 +31,13 @@ void process_number(char *number, int i, int n, ELEMENT *element, NODE *node) {
     node->element = element;
   }
 
-  //printf("number is %d\n", element->number);
-  //printf("type is %d\n", element->type);
+  printf("element is %p, number is %d, type is %d\n", element, element->number, element->type);
 }
 
 //(+ 1 (+ 2 3))
 //(+ 1 (- 3 2) (* 2 3) (/ 6 2))
 NODE *parse(FILE *in, ELEMENT *previous_element) {
+  printf("\nloop start\n");
   int c;
   int n = 0;
   int state = OUT;
@@ -57,7 +58,7 @@ NODE *parse(FILE *in, ELEMENT *previous_element) {
         if(n == 0) {
           operator[i] = '\0';
           current_node->operator = operator;
-
+          printf("operator is %s\n", operator);
         } else {
           process_number(number, i, n, current_element, current_node);
         }
@@ -68,6 +69,7 @@ NODE *parse(FILE *in, ELEMENT *previous_element) {
       state = OUT;
       break;
     case ')':
+      printf("previous element is %p, state is %d\n", previous_element, state);
       previous_element->next = NULL;
       if(state == IN) {
         process_number(number, i, n, current_element, current_node);
@@ -75,6 +77,7 @@ NODE *parse(FILE *in, ELEMENT *previous_element) {
         n = n + 1;
         i = 0;
       }
+      state = END;
       break;
     default:
       if(n == 0) {// operator
@@ -90,24 +93,33 @@ NODE *parse(FILE *in, ELEMENT *previous_element) {
           }
 
           previous_element = current_element;
+          printf("previous element is %p, current_element is %p\n", previous_element, current_element);
         }
 
         if(c == '(') {
           NODE *child = parse(in, current_element);
           current_element->type = 2;
           current_element->node = child;
+          printf("\nelement is %p, node is %p, type is %d\n", current_element, current_element->node, current_element->type);
         } else {
           number[i] = c;
           i = i + 1;
         }
       }
 
-      state = IN;
+      if(c == '(') {
+        state = OUT;
+      } else {
+        state = IN;
+      }
     }
 
-    //printf("operator is %s\n", operator);
+    if(state == END) {
+      break;
+    }
   }
 
+  printf("loop end\n");
   return current_node;
 }
 
@@ -127,12 +139,11 @@ int compute(char *operator, int operand1, int operand2) {
 }
 
 int compute_node_element(ELEMENT *element, char *operator) {
-  //printf("element is %p\n", element);
-  //printf("type is %d\n", element->type);
-  //printf("next is %p\n", element->next);
+  printf("\nelement is %p, type is %d, next is %p\n", element, element->type, element->next);
+
   if(element->next == NULL) {
     if(element->type == 1) {
-      //printf("number -- 1 -- %d\n", element->number);
+      printf("number -- 1 -- %d\n", element->number);
       return element->number;
     } else {
       return compute_node_element(element->node->element,
@@ -140,7 +151,7 @@ int compute_node_element(ELEMENT *element, char *operator) {
     }
   } else {
     if(element->type == 1) {
-      //printf("number -- 2 -- %d\n", element->number);
+      printf("number -- 2 -- %d\n", element->number);
       return compute(operator,
                      element->number,
                      compute_node_element(element->next,
@@ -168,7 +179,11 @@ int main(void) {
       //printf("operator is %s\n", root->operator);
       //printf("element of root is %p\n", root->element);
       int result = compute_node_element(root->element, root->operator);
-      printf("%d\n", result);
+
+      c = getc(stdin);
+      if(c == 10) {
+        printf("%d\n", result);
+      }
     }
   }
 }
